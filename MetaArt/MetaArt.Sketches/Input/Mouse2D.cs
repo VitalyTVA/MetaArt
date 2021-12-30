8d22100c3d3e8529133cbe97873f42c3ec4bd13f
vitalytva <vitalytva@yandex.ru>
2021-12-30 19:22:53 +0300

    Mouse2D

---
+++ MetaArt/MetaArt.Sketches/Input/Mouse2D.cs
@@ -0,0 +1,24 @@
+﻿namespace Input;
+class Mouse2D : SketchBase {
+    /**
+     * Mouse 2D. 
+     * 
+     * Moving the mouse changes the position and size of each box. 
+     */
+
+    void setup() {
+        size(640, 360);
+        noStroke();
+        rectMode(CENTER);
+    }
+
+    void draw() {
+        background(51);
+        fill(255, 204);
+        rect(mouseX, height / 2, mouseY / 2 + 10, mouseY / 2 + 10);
+        fill(255, 204);
+        float inverseX = width - mouseX;
+        float inverseY = height - mouseY;
+        rect(inverseX, height / 2, (inverseY / 2) + 10, (inverseY / 2) + 10);
+    }
+}
