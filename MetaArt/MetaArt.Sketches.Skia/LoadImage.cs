b6a01e592d7a13428255b8841662581bfc712cc8
vitalytva <vitalytva@yandex.ru>
2022-01-02 12:44:34 +0300

    LoadImage

---
+++ MetaArt/MetaArt.Sketches.Skia/LoadImage.cs
@@ -0,0 +1,16 @@
+﻿using MetaArt;
+using SkiaSharp;
+using System.Diagnostics;
+
+class LoadImage {
+    void setup() {
+        size(640, 360);
+    }
+
+    void draw() {
+        Canvas.Clear(SKColors.Black);  
+        var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("MetaArt.Sketches.Skia.Assets.texture.png");
+        using var image = SKImage.FromEncodedData(stream);
+        Canvas.DrawImage(image, 100, 100, new SKPaint() { Color = SKColors.Black.WithAlpha(70) });
+    }
+}
