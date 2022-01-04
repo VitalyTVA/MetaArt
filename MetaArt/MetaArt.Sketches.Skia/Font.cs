9b5907463fef0c239130aca595d002d9d628500f
vitalytva <vitalytva@yandex.ru>
2022-01-04 16:01:19 +0300

    font experiments

--- MetaArt/MetaArt.Sketches.Skia/Font.cs
+++ MetaArt/MetaArt.Sketches.Skia/Font.cs
@@ -3,39 +3,52 @@ using SkiaSharp;
 using System.Diagnostics;
 
 class Font {
+    String message = "Tickleg";
+    float x, y; // X and Y coordinates of text
+    float hr;  // horizontal and vertical radius of the text
+
+    SKTypeface typeface = null!;
     void setup() {
-        size(800, 400);
+        size(640, 360);
+
+        //hr = textWidth(message) / 2;
+        //println(textWidth(message));
+        //println(textAscent());
+        //println(textDescent());
+        //vr = (textAscent() + textDescent()) / 2;
+        noStroke();
+        x = width / 2;
+        y = height / 2;
+        var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("MetaArt.Sketches.Skia.Assets.SourceCodePro-Regular.ttf");
+        typeface = SKTypeface.FromStream(stream);
     }
 
     void draw() {
-        Canvas.DrawColor(SKColors.White);
-        var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("MetaArt.Sketches.Skia.Assets.SourceCodePro-Regular.ttf");
-        using var typeface = SKTypeface.FromStream(stream);
-        using(SKPaint p = new SKPaint { IsAntialias = true, StrokeWidth = 2, Typeface = typeface, TextSize = 150, TextAlign = SKTextAlign.Center }) {
-            float left = 40;
-            float midCanvasY = height / 2;
-            float zeroHeight = 0;
-            const string Text = "Test String!!!";
-            for(int i = 0; i < 10; i++) {
-                string text = "" + Text[i];
-                SKRect textBounds = SKRect.Empty;
-                p.Color = SKColors.Black;
-                p.Style = SKPaintStyle.Fill;
-                p.MeasureText(text, ref textBounds);
-                if(i == 0)
-                    zeroHeight = textBounds.Height;
-                Console.WriteLine("" + i + ": " + textBounds.Height);
-                SKPoint textLoc = new SKPoint(left, midCanvasY);
-                Canvas.DrawText(text, textLoc.X, textLoc.Y + textBounds.Height / 2, p);
-                left += textBounds.Width;
-            }
-            float topZeroY = midCanvasY - zeroHeight / 2;
-            float bottomZeroY = midCanvasY + zeroHeight / 2;
-            p.Color = SKColors.Red;
-            p.Style = SKPaintStyle.Stroke;
-            Canvas.DrawLine(0, topZeroY, width, topZeroY, p);
-            Canvas.DrawLine(0, midCanvasY, width, midCanvasY, p);
-            Canvas.DrawLine(0, bottomZeroY, width, bottomZeroY, p);
-        }
+        Canvas.Clear(SKColors.Black);
+        var paint = new SKPaint {
+            Color = SKColors.White,
+            TextSize = 100,
+            TextScaleX = 1,
+            TextSkewX = 0,
+            TextAlign = SKTextAlign.Center,
+            IsAntialias = true,
+            Typeface = typeface,
+        };
+        SKRect bounds = new SKRect();
+        hr = paint.MeasureText(message, ref bounds) / 2;
+
+        fill(204, 120);
+        rect(0, 0, width, height);
+
+        fill(0);
+        Canvas.DrawText(message, x, y + (- paint.FontMetrics.Ascent) / 2, paint);
+
+        noFill();
+        stroke(255);
+        strokeWeight(1);
+        line(width / 2 - hr, 0, width / 2 - hr, height);
+        line(width / 2 + hr, 0, width / 2 + hr, height);
+        line(0, height / 2 + 100 / 2 + paint.FontMetrics.Ascent, width, height / 2 + 100 / 2 + paint.FontMetrics.Ascent);
+        line(0, height / 2 + 100 / 2 + paint.FontMetrics.Descent, width, height / 2 + 100 / 2 + paint.FontMetrics.Descent);
     }
 }
