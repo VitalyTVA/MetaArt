091328a3ae369730deac47973c7a5be2f90860d2
vitalytva <vitalytva@yandex.ru>
2022-01-15 11:44:51 +0300

    save window position/location and current sketch

--- MetaArt/MetaArt.Wpf.App/SketchesWindow.xaml.cs
+++ MetaArt/MetaArt.Wpf.App/SketchesWindow.xaml.cs
@@ -38,17 +38,49 @@ namespace MetaArt.Wpf {
             .ToList());
 
             btn.Focus();
-            Closed+= async (o, e) => await img.Stop();
+            Closed += async (o, e) => {
+                SaveState();
+                await img.Stop();
+            };
 
             ICollectionView view = CollectionViewSource.GetDefaultView(sketches);
             view.GroupDescriptions.Add(new PropertyGroupDescription(nameof(SketchDisplayInfo.Category)));
 
             list.ItemsSource = view;
             //list.SelectedIndex = 0;
-            list.SelectionChanged += (o, e) => { img.Run(((SketchDisplayInfo)list.SelectedItem).Type); };
+            list.SelectionChanged += (o, e) => { img.Run(GetCurrentSketch()!.Type); };
             IsVisibleChanged += SketchesWindow_IsVisibleChanged;
 
+            LoadState();
         }
+
+        SketchDisplayInfo? GetCurrentSketch() {
+            return list.SelectedItem as SketchDisplayInfo;
+        }
+
+        void SaveState() {
+            Settings.Default.WindowSize = new System.Drawing.Size((int)Width, (int)Height);
+            Settings.Default.WindowLocation = new System.Drawing.Point((int)Left, (int)Top);
+            Settings.Default.CurrentSketchTypeFullName = GetCurrentSketch()?.Type.FullName;
+            Settings.Default.Save();
+        }
+
+        void LoadState() {
+            var size = Settings.Default.WindowSize;
+            if(!size.IsEmpty) {
+                Width = size.Width;
+                Height = size.Height;
+            }
+            var location = Settings.Default.WindowLocation;
+            Left = location.X;
+            Top = location.Y;
+
+            var sketchTypeFullName = Settings.Default.CurrentSketchTypeFullName;
+            Dispatcher.BeginInvoke(new Action(() => {
+                list.SelectedItem = list.ItemsSource.Cast<SketchDisplayInfo>().FirstOrDefault(x => x.Type.FullName == sketchTypeFullName);
+            }), DispatcherPriority.ContextIdle);
+        }
+
         protected override void OnActivated(EventArgs e) {
             base.OnActivated(e);
             img.UpdateLocation();
