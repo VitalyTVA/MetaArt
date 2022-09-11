eafe65074992a7a93aae1c2a75b882b63f923255
vitalytva <vitalytva@yandex.ru>
2022-09-11 14:51:44 +0400

    extract utilitis to separate repo

--- MetaArt/MetaArt.Core/Utils.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MetaArt.Core {
-    public static class Utils {
-        public static Stream GetStream(Type typeFromAssembly, string filename) {
-            return typeFromAssembly.Assembly.GetManifestResourceStream(typeFromAssembly.Assembly.GetName().Name + ".Assets." + filename);
-        }
-    }
-}
