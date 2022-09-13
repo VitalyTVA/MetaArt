777ba1466b51f939d58d7ca289726c9be14f5691
vitalytva <vitalytva@yandex.ru>
2022-09-13 22:19:57 +0400

    scripts

--- Build.bat
+++ Build.bat
@@ -3,25 +3,21 @@
 echo.
 echo METACORE
 cd ..\MetaCore\src
-dotnet restore
 dotnet build --configuration Debug
 
 echo.
 echo METAART
 cd ..\..\MetaArt\MetaArt
-dotnet restore
 dotnet build --configuration Debug
 
 echo.
 echo METABUTTON
 cd ..\..\MetaButton\src
-dotnet restore
 dotnet build --configuration Debug
 
 echo.
 echo METAART_VIEWERS
 cd ..\..\MetaArt\MetaArt.Viewers
-dotnet restore MetaArt.Viewers.sln
 dotnet build --configuration Debug MetaArt.Viewers.sln
 
 cd ..\..\MetaArt
