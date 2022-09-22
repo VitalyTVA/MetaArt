33bbb12bb8e8f0d75a9357b21f08a834aada39ad
vitalytva <vitalytva@yandex.ru>
2022-09-23 00:29:40 +0400

    SketchView

--- MetaArt.Viewers/MetaArt.Xamarin/MetaArt.Xamarin/SkecthPage.xaml.cs
+++ MetaArt.Viewers/MetaArt.Xamarin/MetaArt.Xamarin/SkecthPage.xaml.cs
@@ -18,130 +18,53 @@ public partial class SkecthPage : ContentPage
         {
             sketch = value;
             OnPropertyChanged();
+            if(Sketch != null)
+                ShowSketch(Sketch!.Current);
         }
     }
 
-    Painter? painter;
 
     public SkecthPage()
 	{
 		InitializeComponent();
-        if (Device.RuntimePlatform == Device.iOS) {
-            this.view.HasRenderLoop = true;
-        }
-        this.view.PaintSurface += (o, e) => {
-            if (Sketch == null) return;
-            if (painter == null) {
-                var info = Sketch!.Current;
-                ShowSketch(info);
-                e.Surface.Canvas.Clear(SKColors.Black);
-                return;
-            }
-
-            painter?.PaintSurface(e.Surface);
-        };
-        
-        this.view.EnableTouchEvents = true;
-        this.view.Touch += (o, e) => {
-            if (e.ActionType == SkiaSharp.Views.Forms.SKTouchAction.Pressed) {
-                //painter?.OnMouseOver(e.Location.X, e.Location.Y);
-                painter?.OnMouseDown(e.Location.X, e.Location.Y, isLeft: false);
-            }
-            if (e.ActionType == SkiaSharp.Views.Forms.SKTouchAction.Moved) painter?.OnMouseOver(e.Location.X, e.Location.Y);
-            if (e.ActionType == SkiaSharp.Views.Forms.SKTouchAction.Released) {
-                painter?.OnMouseUp(e.Location.X, e.Location.Y);
-                painter?.OnMouseLeave();
-            }
-            e.Handled = true;
-
-        };
+        view.Feedback += (o, feedback) => fpsLabel.Text = ((int)feedback.DrawTime.TotalMilliseconds).ToString();
     }
 
-    SketchDisplayInfo? currentSketch;
-    private void ShowSketch(SketchDisplayInfo info)
+    void ShowSketch(SketchDisplayInfo info)
     {
         Preferences.Set("SketchName", info.Name);
-        painter?.Dispose();
-
-        painter = new Painter(
-            info.CreateSketch(),
-            () => {
-                if (Device.RuntimePlatform == Device.Android) {
-                    //this.view.InvalidateSurface();
-                    this.Dispatcher.BeginInvokeOnMainThread(this.view.InvalidateSurface);
-                } else {
-                    //this.view.InvalidateSurface();
-                }
-            },
-            feedback => {
-                if (Device.RuntimePlatform == Device.iOS) {
-                    fpsLabel.Text = ((int)feedback.DrawTime.TotalMilliseconds).ToString();
-                }
-            },
-            displayDensity: (float)DeviceDisplay.MainDisplayInfo.Density,
-            deviceType: DeviceType.Mobile,
-            createSoundFile: CreateSoundFile,
-            getValue: name => Preferences.Get("Sketch_" + name, default(string)),
-            setValue: (name, value) => Preferences.Set("Sketch_" + name, value)
-        );
-        painter.SetSize((int)view.CanvasSize.Width, (int)view.CanvasSize.Height);
-        painter.Setup();
-
-        this.view.InvalidateSurface();
-        this.Dispatcher.BeginInvokeOnMainThread(() => {
-            this.title.Text = info.Name;
-        });
-        currentSketch = info;
+        view.SetSketch(info.CreateSketch());
+        title.Text = info.Name;
     }
-    class XamarinSoundFile : SoundFile {
-        readonly ISimpleAudioPlayer player;
 
-        public XamarinSoundFile(ISimpleAudioPlayer player) {
-            this.player = player;
-        }
-        public override void play() {
-            player.Play();
-        }
-    }
-    static SoundFile CreateSoundFile(Stream stream) {
-        ISimpleAudioPlayer player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
-        player.Load(stream);
-        return new XamarinSoundFile(player);
-    } 
     //protected override void OnNavigatedFrom(NavigationEventArgs e) {
     //    base.OnNavigatedFrom(args);
 
     //}
     protected override bool OnBackButtonPressed() {
-        DisposePainter();
-        this.Sketch = null;
+        view.ClearSkecth();
         return base.OnBackButtonPressed();
     }
 
-    private void DisposePainter() {
-        painter?.Dispose();
-        painter = null;
-    }
+
 
     void Button_Clicked(System.Object sender, System.EventArgs e) {
         var list = Sketch!.Lists;
-        var index = list.IndexOf(currentSketch!) - 1;
+        var index = list.IndexOf(Sketch.Current!) - 1;
         if (index < 0) index = list.Count - 1;
-        DisposePainter();
+        view.ClearSkecth();
         this.Sketch = new SketchNavInfo(list[index], this.Sketch!.Lists);
-        view.InvalidateSurface();
-        //ShowSketch(list[index]);
+        ShowSketch(Sketch.Current);
     }
 
     void Button_Clicked_1(System.Object sender, System.EventArgs e)
     {
         var list = Sketch!.Lists;
-        var index = list.IndexOf(currentSketch!) + 1;
+        var index = list.IndexOf(Sketch.Current!) + 1;
         if (index >= list.Count) index = list.Count;
-        DisposePainter();
+        view.ClearSkecth();
         this.Sketch = new SketchNavInfo(list[index], this.Sketch!.Lists);
-        view.InvalidateSurface();
-        //ShowSketch(list[index]);
+        ShowSketch(Sketch.Current);
     }
 }
 
--- MetaArt.Viewers/MetaArt.Xamarin/MetaArt.Xamarin/SketchView.xaml.cs
+++ MetaArt.Viewers/MetaArt.Xamarin/MetaArt.Xamarin/SketchView.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using MetaArt.Skia;
+using Plugin.SimpleAudioPlayer;
+using SkiaSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +15,95 @@ namespace MetaArt.Xamarin {
     public partial class SketchView : ContentView {
         public SketchView() {
             InitializeComponent();
-        }
-        public void SetSketch(object sketch) { 
+            if(Device.RuntimePlatform == Device.iOS) {
+                this.view.HasRenderLoop = true;
+            }
+            this.view.PaintSurface += (o, e) => {
+                if(sketch == null) return;
+                if(painter == null) {
+                    ShowSketch();
+                    e.Surface.Canvas.Clear(SKColors.Black);
+                    return;
+                }
 
+                painter?.PaintSurface(e.Surface);
+            };
+
+            this.view.EnableTouchEvents = true;
+            this.view.Touch += (o, e) => {
+                if(e.ActionType == SkiaSharp.Views.Forms.SKTouchAction.Pressed) {
+                    //painter?.OnMouseOver(e.Location.X, e.Location.Y);
+                    painter?.OnMouseDown(e.Location.X, e.Location.Y, isLeft: false);
+                }
+                if(e.ActionType == SkiaSharp.Views.Forms.SKTouchAction.Moved) painter?.OnMouseOver(e.Location.X, e.Location.Y);
+                if(e.ActionType == SkiaSharp.Views.Forms.SKTouchAction.Released) {
+                    painter?.OnMouseUp(e.Location.X, e.Location.Y);
+                    painter?.OnMouseLeave();
+                }
+                e.Handled = true;
+
+            };
+
+        }
+        public event EventHandler<PaintFeedback>? Feedback;
+        object? sketch;
+        Painter? painter;
+        public void SetSketch(object sketch) {
+            this.sketch = sketch;
+            view.InvalidateSurface();
+        }
+
+        void ShowSketch() { 
+            painter?.Dispose();
+            painter = new Painter(
+                sketch!,
+                () => {
+                    if(Device.RuntimePlatform == Device.Android) {
+                    //this.view.InvalidateSurface();
+                    this.Dispatcher.BeginInvokeOnMainThread(this.view.InvalidateSurface);
+                    } else {
+                    //this.view.InvalidateSurface();
+                }
+                },
+                feedback => {
+                    if(Device.RuntimePlatform == Device.iOS) {
+                        Feedback?.Invoke(this, feedback);
+                    }
+                },
+                displayDensity: (float)DeviceDisplay.MainDisplayInfo.Density,
+                deviceType: DeviceType.Mobile,
+                createSoundFile: CreateSoundFile,
+                getValue: name => Preferences.Get("Sketch_" + name, default(string)),
+                setValue: (name, value) => Preferences.Set("Sketch_" + name, value)
+            );
+            painter.SetSize((int)view.CanvasSize.Width, (int)view.CanvasSize.Height);
+            painter.Setup();
+
+            this.view.InvalidateSurface();
+        }
+        public void ClearSkecth() {
+            DisposePainter();
+            this.sketch = null;
+        }
+        void DisposePainter() {
+            painter?.Dispose();
+            painter = null;
+        }
+
+        class XamarinSoundFile : SoundFile {
+            readonly ISimpleAudioPlayer player;
+
+            public XamarinSoundFile(ISimpleAudioPlayer player) {
+                this.player = player;
+            }
+            public override void play() {
+                player.Play();
+            }
+        }
+        static SoundFile CreateSoundFile(Stream stream) {
+            ISimpleAudioPlayer player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+            player.Load(stream);
+            return new XamarinSoundFile(player);
         }
     }
 }
