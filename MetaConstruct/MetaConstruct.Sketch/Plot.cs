e64e0c924615aa2844e01e438219d4108bb179a3
vitalytva <vitalytva@yandex.ru>
2022-10-31 16:52:47 +0400

    Pentaspiral

--- MetaConstruct/MetaConstruct.Sketch/Plot.cs
+++ MetaConstruct/MetaConstruct.Sketch/Plot.cs
@@ -76,7 +76,9 @@ static class PlotsHelpers {
             RegisterPlot(Test1),
             RegisterPlot(Test2),
             RegisterPlot(Test3),
+            RegisterPlot(Bisection),
             RegisterPlot(Pentagon),
+            RegisterPlot(Pentaspiral),
             RegisterPlot(SixCircles),
         };
     static (Func<Constructor, Surface, PlotInfo>, string) RegisterPlot(Func<Constructor, Surface, PlotInfo> action, [CallerArgumentExpression("action")] string name = "") {
@@ -172,34 +174,90 @@ static class PlotsHelpers {
         );
     }
 
+    static PlotInfo Bisection(Constructor c, Surface s) {
+        var p1 = c.Point();
+        var p2 = c.Point();
+
+        c.LineSegment(p1, p2).Add(s, DisplayStyle.Visible);
+        var (bisection, (c1, c2)) = c.Bisection(p1, p2);
+        bisection.Add(s);
+        c1.Add(s);
+        c2.Add(s);
+
+        return new PlotInfo(
+            new[] {
+                (p1, new Vector2(300, 400)),
+                (p2, new Vector2(500, 400)),
+            }
+        );
+    }
+
     static PlotInfo Pentagon(Constructor c, Surface s) {
         var center = c.Point();
         var vertex0 = c.Point();
-        var circle = c.Circle(center, vertex0).Add(s);
-        var line0 = c.Line(center, vertex0).Add(s);
 
-        var bisection1 = PlotPrimitives.Bisection(c, vertex0, c.Intersect(line0, circle).Point2).Add(s);
+        var ((vertex1, vertex2, vertex3, vertex4), primitives) = c.Pentagon(center, vertex0);
 
-        var bisection2 = PlotPrimitives.Bisection(c, center, c.Intersect(bisection1, circle).Point2);
-        c.LineSegment(bisection2, circle).Add(s, DisplayStyle.Background);
-        var circle1 = c.Circle(c.Intersect(bisection1, bisection2), vertex0);
-        c.CircleSegment(circle1, bisection1).Add(s, DisplayStyle.Background);
+        primitives.circle.Add(s);
+        primitives.diameter.Add(s);
+        primitives.bisection1.Add(s);
 
-        var intersection = c.Intersect(bisection1, circle1);
-
-        var circle2 = c.Circle(vertex0, intersection.Point1);
-        c.CircleSegment(circle2, circle).Add(s, DisplayStyle.Background);
-        var (vertex4, vertex1) = c.Intersect(circle, circle2);
-
-        var circle3 = c.Circle(vertex0, intersection.Point2);
-        c.CircleSegment(circle3, circle).Add(s, DisplayStyle.Background);
-        var (vertex3, vertex2) = c.Intersect(circle, circle3);
+        c.LineSegment(primitives.bisection2, primitives.circle).Add(s, DisplayStyle.Background);
+        c.CircleSegment(primitives.circle1, primitives.bisection1).Add(s, DisplayStyle.Background);
+        c.CircleSegment(primitives.circle2, primitives.circle).Add(s, DisplayStyle.Background);
+        c.CircleSegment(primitives.circle3, primitives.circle).Add(s, DisplayStyle.Background);
 
         c.LineSegment(vertex0, vertex1).Add(s);
         c.LineSegment(vertex1, vertex2).Add(s);
         c.LineSegment(vertex2, vertex3).Add(s);
         c.LineSegment(vertex3, vertex4).Add(s);
         c.LineSegment(vertex4, vertex0).Add(s);
+
+        return new PlotInfo(
+            new[] {
+                (center, new Vector2(400, 400)),
+                (vertex0, new Vector2(400, 300)),
+            }
+        );
+    }
+
+    static PlotInfo Pentaspiral(Constructor c, Surface s) {
+        var center = c.Point();
+        var vertex0 = c.Point();
+
+        LineSegment[] MakePentagram(Point v0, Point v1, Point v2, Point v3, Point v4) => new[] {
+            c.LineSegment(v0, v2).Add(s, DisplayStyle.Background),
+            c.LineSegment(v2, v4).Add(s, DisplayStyle.Background),
+            c.LineSegment(v4, v1).Add(s, DisplayStyle.Background),
+            c.LineSegment(v1, v3).Add(s, DisplayStyle.Background),
+            c.LineSegment(v3, v0).Add(s, DisplayStyle.Background),
+        };
+
+        var ((vertex1, vertex2, vertex3, vertex4), _) = c.Pentagon(center, vertex0);
+        var segments = MakePentagram(vertex0, vertex1, vertex2, vertex3, vertex4);
+
+        var vertex0_ = c.Intersect(c.Line(vertex0, vertex4), c.Line(vertex1, vertex2));
+        var ((vertex1_, vertex2_, vertex3_, vertex4_), _) = c.Pentagon(center, vertex0_);
+        var segments_ = MakePentagram(vertex0_, vertex1_, vertex2_, vertex3_, vertex4_);
+
+        for(int i = 0; i < 5; i++) {
+            var s1 = segments[(i + 1) % 5];
+            var s2_ = segments_[(i + 2) % 5];
+
+            var s4 = segments[(i + 4) % 5];
+            var line = c.Line(s2_.To, center).AsLineSegment().Add(s, DisplayStyle.Background);
+
+            var center_ = c.Intersect(s1.Line, s4.Line);
+            var circle = c.Circle(center_, segments[i].From);
+            var inersection = c.Intersect(line.Line, circle);
+            CircleSegment(circle, circle.Point, inersection.Point2).Add(s);
+        }
+        for(int i = 0; i < 5; i ++) {
+            var circle = c.Circle(segments[(i + 3) % 5].From, segments_[i].From);
+            var line = c.Line(segments_[i].From, center);
+            var inersection = c.Intersect(line, circle);
+            CircleSegment(circle, circle.Point, inersection.Point2).Add(s);
+        }
 
         return new PlotInfo(
             new[] {
@@ -256,5 +314,42 @@ public static class CanvasExtensions {
 }
 
 static class PlotPrimitives {
-    public static Line Bisection(this Constructor c, Point p1, Point p2) => c.AsLine(c.Intersect(c.Circle(p1, p2), c.Circle(p2, p1)));
+    public static (Line bisection, (Circle c1, Circle c2) primitives) Bisection(this Constructor c, Point p1, Point p2) {
+        var c1 = c.Circle(p1, p2);
+        var c2 = c.Circle(p2, p1);
+        return (c.AsLine(c.Intersect(c1, c2)), (c1, c2));
+    }
+    //public static Point Middle(this Constructor c, Point p1, Point p2) {
+    //    var bisection = c.Bisection(p1, p2).bisection;
+    //    return c.Intersect(bisection, c.Line(p1, p2));
+    //}
+
+    public static 
+        (
+            (Point vertex1, Point vertex2, Point vertex3, Point vertex4) result, 
+            (Circle circle, Line diameter, Line bisection1, Line bisection2, Circle circle1, Circle circle2, Circle circle3) primitives
+        )
+        Pentagon(this Constructor c, Point center, Point vertex0) {
+        var circle = c.Circle(center, vertex0);
+        var diameter = c.Line(center, vertex0);
+
+        var bisection1 = c.Bisection(vertex0, c.Intersect(diameter, circle).Point2).bisection;
+
+        var bisection2 = c.Bisection(center, c.Intersect(bisection1, circle).Point2).bisection;
+        c.LineSegment(bisection2, circle);
+        var circle1 = c.Circle(c.Intersect(bisection1, bisection2), vertex0);
+        c.CircleSegment(circle1, bisection1);
+
+        var intersection = c.Intersect(bisection1, circle1);
+
+        var circle2 = c.Circle(vertex0, intersection.Point1);
+        c.CircleSegment(circle2, circle);
+        var (vertex4, vertex1) = c.Intersect(circle, circle2);
+
+        var circle3 = c.Circle(vertex0, intersection.Point2);
+        c.CircleSegment(circle3, circle);
+        var (vertex3, vertex2) = c.Intersect(circle, circle3);
+        
+        return ((vertex1, vertex2, vertex3, vertex4), (circle, diameter, bisection1, bisection2, circle1, circle2, circle3));
+    }
 }
