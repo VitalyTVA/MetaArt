df283c5b933f2e4f134608c3fa9a67dfdd67bc87
vitalytva <vitalytva@yandex.ru>
2022-12-01 00:13:39 +0400

    refactoring

--- MetaConstruct/MetaConstruct.Sketch/Plot.cs
+++ MetaConstruct/MetaConstruct.Sketch/Plot.cs
@@ -24,68 +24,26 @@ class Plot {
             size(800, 800);
         //fullRedraw();
 
-        void SetStyle(DisplayStyle style) {
-            stroke(color(style == DisplayStyle.Background ? 70 : 255));
-            strokeWeight(1);
-        }
-
-        painter = new Painter(
-            DrawCircle: (circle, style) => {
-                SetStyle(style);
-                noFill();
-                Sketch.circle(circle.center.X, circle.center.Y, circle.radius * 2);
-            },
-            DrawCircleSegment: (segment, style) => {
-                SetStyle(style);
-                noFill();
-                arc(segment.circle.center.X, segment.circle.center.Y, segment.circle.radius * 2, segment.circle.radius * 2, segment.from, segment.to);
-            },
-            DrawLine: (l, style) => {
-                var v = Vector2.Normalize(l.from - l.to);
-                var p1 = l.to + v * (width + height);
-                var p2 = l.from - v * (width + height);
-                SetStyle(style);
-                noFill();
-                line(p1.X, p1.Y, p2.X, p2.Y);
-            },
-            DrawLineSegment: (l, style) => {
-                SetStyle(style);
-                noFill();
-                line(l.from.X, l.from.Y, l.to.X, l.to.Y);
-            },
-            FillContour: (segments, style) => {
-                noStroke();
-                fill(White);
-                beginShape();
-                foreach(var segment in segments.Select(x => x.ToLeft())) {
-                    arcVertex(segment.circle.center.X, segment.circle.center.Y, segment.circle.radius * 2, segment.circle.radius * 2, segment.from, segment.to);
-                }
-                endShape(CLOSE);
-            },
-            DrawPoint: (p, kind, style) => {
-                SetStyle(style);
-                if(kind == PointKind.Free) {
-                    stroke(255, 255, 0);
-                }
-                strokeWeight(4);
-                point(p.X, p.Y);
-            }
-        );
+        CreatePainter();
 
         controller = new PlotController((int)(width / displayDensity()), (int)(height / displayDensity()));
 
         var fileNameCaption = caption("File");
+
         string? fileName = null;
+        void SetCaption() {
+            fileNameCaption.Text = fileName != null ? Path.GetFileName(fileName) : null;
+        }
         var surface = CreateSurface();
         if(getPlot != null) {
             var info = getPlot(surface.Constructor, surface);
             surface.SetPoints(info.Points);
+            controller.Load(surface);
         } else {
             fileName = readValue("fileName");
             LoadFile();
-            fileNameCaption.Text = Path.GetFileName(fileName);
         }
-        controller.Load(surface);
+        SetCaption();
 
 
 
@@ -134,14 +92,15 @@ class Plot {
         void SaveFileName() {
             if(getPlot == null)
                 writeValue("fileName", fileName);
+            SetCaption();
             fileNameCaption.Text = Path.GetFileName(fileName);
         }
         void LoadFile() {
             surface = CreateSurface();
             using var stream = File.Open(fileName, FileMode.Open, FileAccess.Read);
             Serialization.SurfaceInfo.Deserialize(surface, stream);
-            controller.Load(surface);
             SaveFileName();
+            controller.Load(surface);
         }
         command(
             exectute: () => {
@@ -164,6 +123,56 @@ class Plot {
             },
             caption: "Open",
             shortCut: ('o', ModifierKeys.Ctrl)
+        );
+    }
+
+    private void CreatePainter() {
+        void SetStyle(DisplayStyle style) {
+            stroke(color(style == DisplayStyle.Background ? 70 : 255));
+            strokeWeight(1);
+        }
+
+        painter = new Painter(
+            DrawCircle: (circle, style) => {
+                SetStyle(style);
+                noFill();
+                Sketch.circle(circle.center.X, circle.center.Y, circle.radius * 2);
+            },
+            DrawCircleSegment: (segment, style) => {
+                SetStyle(style);
+                noFill();
+                arc(segment.circle.center.X, segment.circle.center.Y, segment.circle.radius * 2, segment.circle.radius * 2, segment.from, segment.to);
+            },
+            DrawLine: (l, style) => {
+                var v = Vector2.Normalize(l.from - l.to);
+                var p1 = l.to + v * (width + height);
+                var p2 = l.from - v * (width + height);
+                SetStyle(style);
+                noFill();
+                line(p1.X, p1.Y, p2.X, p2.Y);
+            },
+            DrawLineSegment: (l, style) => {
+                SetStyle(style);
+                noFill();
+                line(l.from.X, l.from.Y, l.to.X, l.to.Y);
+            },
+            FillContour: (segments, style) => {
+                noStroke();
+                fill(White);
+                beginShape();
+                foreach(var segment in segments.Select(x => x.ToLeft())) {
+                    arcVertex(segment.circle.center.X, segment.circle.center.Y, segment.circle.radius * 2, segment.circle.radius * 2, segment.from, segment.to);
+                }
+                endShape(CLOSE);
+            },
+            DrawPoint: (p, kind, style) => {
+                SetStyle(style);
+                if(kind == PointKind.Free) {
+                    stroke(255, 255, 0);
+                }
+                strokeWeight(4);
+                point(p.X, p.Y);
+            }
         );
     }
 
