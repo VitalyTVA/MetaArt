58e2e3f099753b848275fb3d0d2d9cd5cea687a4
vitalytva <vitalytva@yandex.ru>
2024-09-08 14:34:11 +0400

    Introduce IFS object

--- MetaArt.Viewers/MetaArt.Sketches/IFS/SerpinskyTriangle.cs
+++ MetaArt.Viewers/MetaArt.Sketches/IFS/SerpinskyTriangle.cs
@@ -1,21 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace IFS;
-class SerpinskyTriangle
-{
-    void setup()
-    {
+class SerpinskyTriangle {
+    void setup() {
         size(800, 800);
         stroke(255);
         strokeWeight(1);
+        var r = 0.5f;
+        ifs = new(
+            Transforms: new[] {
+                   new Vector2(0, 0),
+                   new Vector2(width, 0),
+                   new Vector2(width / 2, height),
+                }
+                .Select(v => IFS.CreateLerpTransform(v, r))
+                .ToArray(),
+            Probabilities: [1 / 3f, 2 / 3f ]
+
+        );
     }
+
+    IFS ifs = default!;
+    Color[] colors = [Colors.Red, Colors.Green, Colors.Blue];
 
     float x = 0;
     float y = 0;
-    void draw()
-    {
+    void draw() {
         translate(0, height);
         scale(1, -1);
 
@@ -25,23 +38,29 @@ class SerpinskyTriangle
             float xn = 0;
             float yn = 0;
             float r = random(1);
-            if(r < 1 / 3f) {
-                xn = x / 2;
-                yn = y / 2;
-                stroke(Colors.Red);
-            } else if(r < 2 / 3f) {
-                xn = (x + width) / 2;
-                yn = y / 2;
-                stroke(Colors.Green);
-            } else {
-                xn = (x + width / 2) / 2;
-                yn = (y + height) / 2;
-                stroke(Colors.Blue);
-            }
+            var (v, i) = ifs.Transform(new Vector2(x, y), r);
+            xn = v.X;
+            yn = v.Y;
+
+            stroke(colors[i]);
             point(round(xn), round(yn));
             x = xn;
             y = yn;
             t++;
         }
+    }
+}
+
+record IFS(Matrix3x2[] Transforms, float[] Probabilities) { 
+    public static Matrix3x2 CreateLerpTransform(Vector2 point, float r) {
+        return new Matrix3x2(r, 0, 0, r, point.X * (1 - r), point.Y * (1 - r));
+    }
+    public (Vector2 point, int index) Transform(Vector2 point, float p) {
+        var i = 0;
+        for(i = 0; i < Probabilities.Length; i++) {
+            if(p < Probabilities[i])
+                break;
+        }
+        return (Vector2.Transform(point, Transforms[i]), i);
     }
 }
